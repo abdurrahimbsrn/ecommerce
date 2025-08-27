f958fb800655b6cf221ed0bb4fe5c61df8407373
abdurrahimbsrn <abdurrahimbasaran@hotmail.com>
2025-08-27 21:21:30 +0300

    d

--- services/urun-service/target/generated-sources/annotations/com/ecommerce/urun_service/mapper/KategoriMapperImpl.java
+++ services/urun-service/target/generated-sources/annotations/com/ecommerce/urun_service/mapper/KategoriMapperImpl.java
@@ -13,8 +13,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-27T10:33:34+0300",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21 (Oracle Corporation)"
+    date = "2025-08-27T19:00:55+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.8 (Ubuntu)"
 )
 @Component
 public class KategoriMapperImpl implements KategoriMapper {
